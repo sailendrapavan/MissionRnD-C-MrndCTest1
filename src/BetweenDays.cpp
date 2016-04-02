d5a2298e888a857e7789f5dec13ab22bec92ee67
sailendrapavan <sailendrapavan@missionrnd.com>
2016-04-02 20:31:39 +0530

    Submit Commit

--- src/BetweenDays.cpp
+++ src/BetweenDays.cpp
@@ -37,38 +37,32 @@ int countLeapYears(Date d)
 {
 	int years = d.y;
 
-	// Check if the current year needs to be considered
-	// for the count of leap years or not
+	
 	if (d.m <= 2)
 		years--;
 
-	// An year is a leap year if it is a multiple of 4,
-	// multiple of 400 and not a multiple of 100.
+	
 	return years / 4 - years / 100 + years / 400;
 }
 int getDifference(Date dt1, Date dt2)
 {
-	// COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1'
-
-	// initialize count using years and day
+	
 	long int n1 = dt1.y * 365 + dt1.d;
 
-	// Add days for months in given date
+	
 	for (int i = 0; i<dt1.m - 1; i++)
 		n1 += monthDays[i];
 
-	// Since every leap year is of 366 days,
-	// Add a day for every leap year
+	
 	n1 += countLeapYears(dt1);
 
-	// SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2'
-
+	
 	long int n2 = dt2.y * 365 + dt2.d;
 	for (int i = 0; i<dt2.m - 1; i++)
 		n2 += monthDays[i];
 	n2 += countLeapYears(dt2);
 
-	// return difference between two counts
+	
 	return (n2 - n1);
 }
 int d1, d2, m1, m2, p1, p2;
@@ -89,7 +83,6 @@ int between_days(struct node *date1head, struct node *date2head){
 	p2 = p2 * 1000 + 100 * (date2head->next->next->next->next->next->data);
 	p2 = p2 + 10 * (date2head->next->next->next->next->next->next->data);
 	p2 = p2 + date2head->next->next->next->next->next->next->next->data;
-	printf("%d%d%d", p1, p2, m1);
 	Date dt1, dt2;
 	dt1.d = d1;
 	dt1.m = m1;
